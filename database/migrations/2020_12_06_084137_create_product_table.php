9c4bb7eab3540433c2751b234de4ddfa47278cc1
vuquangthai2201 <vuquangthai2201@gmail.com>
2020-12-21 16:17:04 +0700

    first commit

---
+++ database/migrations/2020_12_06_084137_create_product_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('product_id');
+            $table->string('product_name');
+            $table->integer('id_type');
+            $table->text('product_desc');
+            $table->float('unit_price');
+            $table->float('promotion_price');
+            $table->string('product_image');
+            $table->string('product_unit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>samystudy</title>
+</head>
+<body>
+<div>
+    
+</div>
+</body>
+</html>
